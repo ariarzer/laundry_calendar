7536366a479582c8a9e20755ffa00f7101de7b51
Sofi Valitova <ariarzer@gmail.com>
2023-02-03 21:43:35 +0400

    Fix date formatter

--- scripts/generateData.js
+++ scripts/generateData.js
@@ -1,7 +1,9 @@
 const weekDayFormatter = new Intl.DateTimeFormat('en-GB', { weekday: 'short'});
 
 function dateFormatter(date) {
-    return `${weekDayFormatter.format(date)}, ${date.getDate().toString().padStart(2, '0')}.${(date.getMonth() + 1).toString().padStart(2, '0')}`
+    const dateString = date.getDate().toString().padStart(2, '0');
+    const monthString = (date.getMonth() + 1).toString().padStart(2, '0');
+    return `${weekDayFormatter.format(date)}, ${dateString}.${monthString}`
 }
 
 const timeFormatter = new Intl.DateTimeFormat('en-GB', { timeStyle: 'short', pattern: "{hour}:{minute}"});
